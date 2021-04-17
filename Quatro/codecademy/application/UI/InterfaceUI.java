fb848838541f2cf54d4e03a0b2f0028ca814b3ca
LiamGev <aneelian@hotmail.com>
2021-04-17 17:01:33 +0200

    Added Interface used for the UI

---
+++ Quatro/codecademy/application/UI/InterfaceUI.java
@@ -0,0 +1,9 @@
+package Quatro.codecademy.application.UI;
+
+import javafx.scene.Scene;
+import Quatro.codecademy.application.controllers.UIController;
+
+public interface InterfaceUI {
+    
+    public Scene getUI(UIController uicontroller);
+}
