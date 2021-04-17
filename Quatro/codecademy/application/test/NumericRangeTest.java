a9b243496db33e2197ef54732cb8cae8acc1e180
Thijs <thijsvanrijsbergen@gmail.com>
2021-04-17 15:58:30 +0200

    Unit test voor percentages

---
+++ Quatro/codecademy/application/test/NumericRangeTest.java
@@ -0,0 +1,60 @@
+package Quatro.codecademy.application.test;
+
+import Quatro.codecademy.application.utils.Validate;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumericRangeTest {
+
+// * @subcontract value within valid range {
+// *   @requires 0 <= percentage <= 100;
+// *   @ensures \result = true;
+// * }
+
+    @Test
+    public void testValidatePercentageRequiresPercentage5EnsuresTrue() {
+        // Arrange percentage
+        int percentage = 7;
+
+        // Validate example percentage
+        boolean result = Validate.validatePercentage(percentage); // Should be true
+
+        // Assert
+        assertEquals(true, result);
+    }
+
+// * @subcontract value out of range low {
+// * @requires percentage < 0;
+// * @ensures \result = false;
+// * }
+
+    @Test
+    public void testValidatePercentageRequiresNegativePercentageEnsuresFalse() {
+        // Arrange percentage
+        int percentage = -10;
+
+        // Validate example percentage
+        boolean result = Validate.validatePercentage(percentage); // Should be false
+
+        // Assert
+        assertEquals(false, result);
+    }
+
+// * @subcontract value out of range high {
+// * @requires percentage > 100;
+// * @signals \result = false;
+// * }
+
+    @Test
+    public void testValidatePercentageRequiresPercentage105EnsuresFalse() {
+        // Arrange percentage
+        int percentage = 122;
+
+        // Validate example percentage
+        boolean result = Validate.validatePercentage(percentage); // Should be false
+
+        // Assert
+        assertEquals(false, result);
+    }
+}
