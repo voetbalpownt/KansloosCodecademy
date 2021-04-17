1043946e0b625f132e3246f4d5af27510ae4478c
Ricardo <ricardo@gmail.com>
2021-04-17 15:08:08 +0200

    fucnties getpercentage,getTop,getCount, toegevoed

---
+++ Quatro/codecademy/application/controllers/CertificateController.java
@@ -0,0 +1,82 @@
+package Quatro.codecademy.application.controllers;
+
+import java.text.DecimalFormat;
+
+import Quatro.codecademy.application.logic.Gender;
+
+public class CertificateController extends Database {
+
+    public CertificateController(String connectionUrl) {
+        super(connectionUrl);
+    }
+
+       // diffrence between completed courses and enrollments
+       public String getPercentage(Gender gender) {
+        try {
+            double count = 0.0;
+            double countcompleted = 0.0;
+            connectDatabase();
+            String SQL = "SELECT COUNT(StudentEmailAddress) AS Count FROM Certificate WHERE StudentEmailAddress IN (SELECT EmailAddress FROM Enrollment WHERE EmailAddress IN(SELECT EmailAddress FROM Student WHERE Gender = \'" + gender + "\'))";
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(SQL);
+            if (resultSet.next()) {
+                countcompleted = resultSet.getInt("Count");
+            }
+            SQL = "SELECT COUNT(EmailAddress) AS Count FROM Enrollment WHERE EmailAddress IN(SELECT EmailAddress FROM Student WHERE Gender = \'"
+                    + gender + "\')";
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(SQL);
+            if (resultSet.next()) {
+                count = resultSet.getInt("Count");
+            }
+            DecimalFormat format = new DecimalFormat("#0.0");
+            String formatted = format.format((countcompleted / count) * 100);
+            if (formatted.equals("NaN")) {
+                return "0";
+            }
+            return formatted;
+        } catch (Exception e) {
+            System.out.println("ERROR:\n\n" + e);
+        }
+        return "0";
+    }
+
+    // get top certificate by coursename
+    public String getTop() {
+        String top = "";
+        try {
+            connectDatabase();
+            String SQL = "SELECT TOP 3 CourseName FROM Certificate GROUP BY CourseName";
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(SQL);
+            int i = 1;
+            if (resultSet.next()) {
+                top = top + i + ": " + resultSet.getString("CourseName") + "\n";
+                i++;
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR:\n\n" + e);
+        }
+        return top;
+    }
+
+    // get count from certificate by coursename
+    public Integer getCount(String courseName) {
+        try {
+            connectDatabase();
+            String SQL = "SELECT COUNT(CourseName) AS Count FROM Certificate WHERE CourseName = \'" + courseName + "\'";
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery(SQL);
+            if (resultSet.next()) {
+                return resultSet.getInt("Count");
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR:\n\n" + e);
+        }
+        return 0;
+    }
+
+ 
+
+}
