146297502a1a695e6567223109ed101bf04f2130
Thijs <thijsvanrijsbergen@gmail.com>
2021-04-17 17:05:20 +0200

    App.java is gelinkt met UIController en MainUI scherm werkt

--- Quatro/codecademy/application/App.java
+++ Quatro/codecademy/application/App.java
@@ -1,9 +1,12 @@
 package Quatro.codecademy.application;
 
+import Quatro.codecademy.application.controllers.UIController;
+import javafx.application.Application;
+
 public class App {
 
     public static void main(String[] args) {
-        
+        Application.launch(UIController.class, args);
     }
 
 }
